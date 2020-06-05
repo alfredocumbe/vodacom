f8eebfe08434c08500dec1c2412599e3bfcd54eb
Alfredo Cumbe <alfredojoaocumbe@gmail.com>
2020-06-05 14:38:12 +0200

    build script added for linux modification

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn clean package && docker build -t de.rieckpil.quickstarts/crud-api-application .
-docker rm -f crud-api-application || true && docker run -d -p 9080:9080 -p 9443:9443 --name crud-api-application de.rieckpil.quickstarts/crud-api-application
+mvn clean package && docker build -t challenge .
+docker rm -f challenge || true && docker run -d -p 9080:9080 -p 9443:9443 --name challenge
